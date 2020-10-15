83c1bd710f6aeb6cb25dabea8a56155f58da761a
mwinamijr <mwinamijr@gmail.com>
2020-10-15 13:30:15 +0300

    added a features component modified home page contents

--- src/containers/HomePage.js
+++ src/containers/HomePage.js
@@ -1,9 +1,12 @@
 import React from "react";
+import Carousel from "../components/Carousel";
+import Services from "../components/Features";
 
 const Home = () =>{
     return(
         <div>
-        <h1>Home</h1>
+            <Carousel />
+            <Services />
         </div>
     )
 }
