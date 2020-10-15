9c6668078e7328146607227a7b93957cdc4d02ce
mwinamijr <mwinamijr@gmail.com>
2020-10-15 15:44:22 +0300

    initialize state fro articles from API

--- src/containers/Blog.js
+++ src/containers/Blog.js
@@ -1,4 +1,5 @@
 import React from "react";
+import axios from 'axios';
 
 import BlogList from "../components/BlogList";
 
@@ -6,6 +7,16 @@ class BlogListPage extends React.Component {
 
   state = {
     articles: []
+  }
+
+  componentDidMount() {
+    axios.get("http://127.0.0.1:8000/articles")
+    .then(res => {
+      this.setState({
+        articles: res.data
+      })
+      console.log(res.data)
+    })
   }
 
   render() {
