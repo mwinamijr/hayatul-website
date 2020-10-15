70e799077a394d7576a13bbceed97990d013a31e
mwinamijr <mwinamijr@gmail.com>
2020-10-15 13:24:03 +0300

    created a carousel components

---
+++ src/components/Carousel.js
@@ -0,0 +1,62 @@
+import React from "react";
+import { MDBCarousel, MDBCarouselCaption, MDBCarouselInner, MDBCarouselItem, MDBView } from
+"mdbreact";
+
+const Carousel = () => {
+  return (
+    <div>
+      <hr />
+      <MDBCarousel
+        activeItem={1}
+        length={3}
+        showControls={true}
+        showIndicators={true}
+        className="z-depth-1"
+      >
+        <MDBCarouselInner>
+          <MDBCarouselItem itemId="1">
+            <MDBView>
+              <img
+                className="d-block w-100"
+                src="https://mdbootstrap.com/img/Photos/Slides/img%20(130).jpg"
+                alt="First slide"
+              />
+            </MDBView>
+            <MDBCarouselCaption>
+            <h3 className="h3-responsive">Slight Mast</h3>
+            <p>Third text</p>
+          </MDBCarouselCaption>
+          </MDBCarouselItem>
+          <MDBCarouselItem itemId="2">
+            <MDBView>
+              <img
+                className="d-block w-100"
+                src="https://mdbootstrap.com/img/Photos/Slides/img%20(129).jpg"
+                alt="Second slide"
+              />
+            </MDBView>
+            <MDBCarouselCaption>
+            <h3 className="h3-responsive">Slight Mast</h3>
+            <p>Third text</p>
+          </MDBCarouselCaption>
+          </MDBCarouselItem>
+          <MDBCarouselItem itemId="3">
+            <MDBView>
+              <img
+                className="d-block w-100"
+                src="https://mdbootstrap.com/img/Photos/Slides/img%20(70).jpg"
+                alt="Third slide"
+              />
+            </MDBView>
+            <MDBCarouselCaption>
+            <h3 className="h3-responsive">Slight Mast</h3>
+            <p>Third text</p>
+          </MDBCarouselCaption>
+          </MDBCarouselItem>
+        </MDBCarouselInner>
+      </MDBCarousel>
+    </div>
+  );
+}
+
+export default Carousel;
