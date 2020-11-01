c46673d46c82e75469ebda19f412ae2f9da69845
mwinamijr <mwinamijr@gmail.com>
2020-11-01 17:50:49 +0300

    created education and health links

--- src/components/navigation/Navbar.js
+++ src/components/navigation/Navbar.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Nav, Navbar } from 'react-bootstrap';
+import { Nav, Navbar, NavDropdown } from 'react-bootstrap';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 import { MDBIcon } from 'mdbreact';
@@ -25,6 +25,14 @@ render() {
             <Nav.Link href="/">Home</Nav.Link>
             <Nav.Link href="/services">Services</Nav.Link>
             <Nav.Link href="/blog">Blog</Nav.Link>
+            <NavDropdown title="Education" id="basic-nav-dropdown">
+              <NavDropdown.Item href="/education/nursery">Nursery</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Primary</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Secondary</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link href="/health">Health</Nav.Link>
             <Nav.Link href="/about">About</Nav.Link>
             <Nav.Link href="/contact">Contact</Nav.Link>
             </Nav>
