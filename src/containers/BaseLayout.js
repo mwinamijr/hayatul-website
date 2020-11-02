2d42ebae91a3aef70a08d547deef6f67d0fa2460
mwinamijr <mwinamijr@gmail.com>
2020-11-02 15:22:52 +0300

    created footer for the page

--- src/containers/BaseLayout.js
+++ src/containers/BaseLayout.js
@@ -1,12 +1,14 @@
 import React from "react";
 
 import TopNavbar from "../components/navigation/Navbar";
+import Footer from "../components/navigation/Footer";
 
 const BaseLayout = (props) => {
 	return(
 		<div className="mt-4">
 			<TopNavbar />
-      { props.children }
+			{ props.children }
+			<Footer />
 		</div>
 		)
 }
