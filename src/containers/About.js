21ed9f61ce668fe1d481984e312d134c0672deae
mwinamijr <mwinamijr@gmail.com>
2020-11-02 14:16:47 +0300

    modified contents of the about page

--- src/containers/About.js
+++ src/containers/About.js
@@ -1,16 +1,20 @@
 import React from "react";
+import { PageHeader } from 'antd';
 import {  MDBRow, MDBCol, MDBIcon } from "mdbreact";
+
+import './styles.css';
 
 const AboutPage = () => {
   return (
       <section className="text-center my-5">
         <h2 className="h1-responsive font-weight-bold my-5">
-          Why is it so great?
+          Ijue taasisi ya Hayatul Islamiya Complex
         </h2>
         <p className="lead grey-text w-responsive mx-auto mb-5">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-          ad minim veniam.
+          Hayatul Islamiya Complex ni taasisi ya kiislamu iliyo chini ya
+          Baraza Kuu la Waislamu Tanzania (BAKWATA). Taasisi hii inamilikiwa kihalali
+          kabisa na wanajumuiya wa Hayatul Islamiya Complex ambao wametoa ahadi mbele ya Allah(S.W.)
+          kuitumikia na kujitolea kikamilifu ili kuweza kupata radhi zake.
         </p>
 
 				<MDBRow>
@@ -22,16 +26,20 @@ const AboutPage = () => {
             />
           </MDBCol>
           <MDBCol lg="7">
+            <PageHeader
+              className="site-page-header"
+              title="Madhumuni ya taasisi"
+            />
+            <br />
             <MDBRow className="mb-3">
               <MDBCol size="1">
                 <MDBIcon icon="share" size="lg" className="indigo-text" />
               </MDBCol>
               <MDBCol xl="10" md="11" size="10">
-                <h5 className="font-weight-bold mb-3">Safety</h5>
+                <h5 className="font-weight-bold mb-3">Elimu</h5>
                 <p className="grey-text">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit
-                  enim ad minima veniam, quis nostrum exercitationem ullam.
-                  Reprehenderit maiores aperiam assumenda deleniti hic.
+                Kujenga, kusimamia na kugharamia uendeshaji wa miradi ya elimu 
+                kuanzia Shule za Awali, za Msingi na za Sekondari.
                 </p>
               </MDBCol>
             </MDBRow>
@@ -40,11 +48,10 @@ const AboutPage = () => {
                 <MDBIcon icon="share" size="lg" className="indigo-text" />
               </MDBCol>
               <MDBCol xl="10" md="11" size="10">
-                <h5 className="font-weight-bold mb-3">Technology</h5>
+                <h5 className="font-weight-bold mb-3">Afya</h5>
                 <p className="grey-text">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit
-                  enim ad minima veniam, quis nostrum exercitationem ullam.
-                  Reprehenderit maiores aperiam assumenda deleniti hic.
+                Kujenga , kusimamia na kugharamia uendeshaji wa miradi ya Afya
+                 kuanzia Zahanati hadi vituo vya afya na hospitali.
                 </p>
               </MDBCol>
             </MDBRow>
@@ -53,11 +60,23 @@ const AboutPage = () => {
                 <MDBIcon icon="share" size="lg" className="indigo-text" />
               </MDBCol>
               <MDBCol xl="10" md="11" size="10">
-                <h5 className="font-weight-bold mb-3">Finance</h5>
+                <h5 className="font-weight-bold mb-3">Udhamini</h5>
                 <p className="grey-text">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit
-                  enim ad minima veniam, quis nostrum exercitationem ullam.
-                  Reprehenderit maiores aperiam assumenda deleniti hic.
+                Kudhamini na kugharamia elimu kwa vijana wa kiislamu wenye uwezo
+                 wa kuendelea na masomo lakini hawana uwezo wa kugharamia elimu hiyo.
+                </p>
+              </MDBCol>
+            </MDBRow>
+            <MDBRow className="mb-3">
+              <MDBCol size="1">
+                <MDBIcon icon="share" size="lg" className="indigo-text" />
+              </MDBCol>
+              <MDBCol xl="10" md="11" size="10">
+                <h5 className="font-weight-bold mb-3">Kilimo na Biashara</h5>
+                <p className="grey-text">
+                Kutafuta na kumiliki maeneo ya kilimo ili kulima mazao ya chakula na mboga
+                 zitzkazotumika katika vitengo vya Jumuiya ili kupunguza gharama za 
+                 uendeshaji katika vitengo hivyo . 
                 </p>
               </MDBCol>
             </MDBRow>
@@ -65,25 +84,17 @@ const AboutPage = () => {
         </MDBRow>
 				<br />
         <MDBRow>
-          <MDBCol md="4">
+          <MDBCol md="6">
             <MDBIcon icon="chart-area" size="3x" className="red-text" />
-            <h5 className="font-weight-bold my-4">Analytics</h5>
+            <h5 className="font-weight-bold my-4">Dira</h5>
             <p className="grey-text mb-md-0 mb-5">
               Lorem ipsum dolor sit amet, consectetur adipisicing elit.
               Reprehenderit maiores aperiam minima assumenda deleniti hic.
             </p>
           </MDBCol>
-          <MDBCol md="4">
+          <MDBCol md="6">
             <MDBIcon icon="book" size="3x" className="cyan-text" />
-            <h5 className="font-weight-bold my-4">Tutorials</h5>
-            <p className="grey-text mb-md-0 mb-5">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-              Reprehenderit maiores aperiam minima assumenda deleniti hic.
-            </p>
-          </MDBCol>
-          <MDBCol md="4">
-            <MDBIcon far icon="comments" size="3x" className="orange-text" />
-            <h5 className="font-weight-bold my-4">Support</h5>
+            <h5 className="font-weight-bold my-4">Dhamira</h5>
             <p className="grey-text mb-md-0 mb-5">
               Lorem ipsum dolor sit amet, consectetur adipisicing elit.
               Reprehenderit maiores aperiam minima assumenda deleniti hic.
