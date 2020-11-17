63983c1f72c67ccf6f80c5fa5afbc1d2d51d2f5c
mwinamijr <mwinamijr@gmail.com>
2020-11-17 13:03:48 +0300

    updateded footer links

--- src/components/navigation/Footer.js
+++ src/components/navigation/Footer.js
@@ -3,30 +3,47 @@ import { MDBCol, MDBContainer, MDBRow, MDBFooter } from "mdbreact";
 
 const FooterPage = () => {
   return (
-    <MDBFooter color="default-color" className="font-small pt-4 mt-4">
+    <MDBFooter color="grey" className="font-small pt-4 mt-4">
       <MDBContainer fluid className="text-center text-md-left">
         <MDBRow>
           <MDBCol md="6">
-            <h5 className="title">Footer Content</h5>
+            <h5 className="title">Hayatul Islmaiya</h5>
             <p>
-              Here you can use rows and columns here to organize your footer
-              content.
+              Pata fursa ya kutembelea sehemu mbalimbali nakujionea mambo yote yanayo patikana
+              haya katika taasisi yaetu.
             </p>
           </MDBCol>
-          <MDBCol md="6">
-            <h5 className="title">Links</h5>
+          <MDBCol md="3">
+            <h5 className="title">School Links</h5>
             <ul>
               <li className="list-unstyled">
-                <a href="#!">Link 1</a>
+                <a href="#!">School Management</a>
               </li>
               <li className="list-unstyled">
-                <a href="#!">Link 2</a>
+                <a href="#!">Shule ya msingi</a>
               </li>
               <li className="list-unstyled">
-                <a href="#!">Link 3</a>
+                <a href="#!">Shule ya sekondari</a>
               </li>
               <li className="list-unstyled">
                 <a href="#!">Link 4</a>
+              </li>
+            </ul>
+          </MDBCol>
+          <MDBCol md="3">
+            <h5 className="title">Links</h5>
+            <ul>
+              <li className="list-unstyled">
+                <a href="https://www.necta.go.tz">NECTA</a>
+              </li>
+              <li className="list-unstyled">
+                <a href="https://www.nacte.go.tz!">NACTE</a>
+              </li>
+              <li className="list-unstyled">
+                <a href="https://www.moe.go.tz">Wizara ya elimu</a>
+              </li>
+              <li className="list-unstyled">
+                <a href="https://www.moh.go.tz">Wizara ya Afya</a>
               </li>
             </ul>
           </MDBCol>
@@ -34,7 +51,7 @@ const FooterPage = () => {
       </MDBContainer>
       <div className="footer-copyright text-center py-3">
         <MDBContainer fluid>
-          &copy; {new Date().getFullYear()} Copyright: <a href="##"> Tech Dome </a>
+          &copy; {new Date().getFullYear()} Copyright: <a href="https://www.mdbootstrap.com"> MDBootstrap.com </a>
         </MDBContainer>
       </div>
     </MDBFooter>
